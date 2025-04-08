dd09429f28c1cbdff4b6b43e24cc2255cd276d4b
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2025-04-08 12:52:41 -0700

    changed network back to oe2, but it needs to be docker_oe2 when you build the OnEarth images from docker compose

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm --mount type=bind,source=$(pwd)/app,target=/app -p 86:80 --net docker_oe2 -e WORKERS_PER_CORE="2" --name onearth-analytics --hostname onearth onearth-analytics
+docker run --rm --mount type=bind,source=$(pwd)/app,target=/app -p 86:80 --net oe2 -e WORKERS_PER_CORE="2" --name onearth-analytics --hostname onearth onearth-analytics
